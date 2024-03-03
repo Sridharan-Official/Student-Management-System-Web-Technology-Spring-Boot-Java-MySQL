2bac443a7a54e165ef1bdd032ffc053fda57e27c
SilambamSri <sridharan2001r@gmail.com>
2024-03-03 16:36:17 +0530

    Changes Updated

--- src/main/java/com/studentdb/sms/StudentManagementSystemApplication.java
+++ src/main/java/com/studentdb/sms/StudentManagementSystemApplication.java
@@ -1,28 +1,15 @@
 package com.studentdb.sms;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.studentdb.sms.entity.Student;
-import com.studentdb.sms.repository.StudentRepository;
-
 @SpringBootApplication
-public class StudentManagementSystemApplication implements CommandLineRunner{
+public class StudentManagementSystemApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
-	@Autowired
-	private StudentRepository studentRepository;
-	
-	@Override
-	public void run(String... args) throws Exception {
-//		Student student1=new Student("slaman", "Khan", "salman@gmail.com");
-//		studentRepository.save(student1);
-		
-	}
+
 
 }
